d3cbef358ae6e5345c70c303b0555293f24ac177
Chris Baldys <36646247+littlestray@users.noreply.github.com>
2020-11-06 16:57:45 -0500

    clock with some styles

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log("main.js")
+
+let initFrame = false;
+let currentTime = new Date()
+let clock;
+
+
+function init() {
+    console.log("init")
+    clock = document.getElementById('clock')
+    return window.requestAnimationFrame(gameLoop)
+}
+
+function update() {
+
+    currentTime = new Date()
+}
+
+function draw() {
+
+
+    clock.innerHTML = currentTime.toString()
+}
+
+
+function gameLoop() {
+    update()
+    draw()
+    window.requestAnimationFrame(gameLoop)
+}
