155a692272a66815c199b66ed98613b5113b4ff1
Chris Baldys <36646247+littlestray@users.noreply.github.com>
2020-11-12 11:22:16 -0500

    blanked for template

--- main.js
+++ main.js
@@ -1,46 +1,17 @@
 let path = window.location + window.path
-let clock, editor
-
-let eventCheckInterval;
 
 console.log(path)
 
 
 function init() {
-
-    eventCheckInterval = setInterval(() => {
-        let dueEvents = editor.timer.check()
-        if (dueEvents.length > 0) {
-
-            for (let i in dueEvents) {
-                console.log(`push this msg: ${dueEvents[i].msg}`)
-                let temp = new Notification(dueEvents[i].msg)
-            }
-
-        }
-    }, 2000)
-
-    clock = document.getElementById("clock")
-    editor = new Editor(
-        new Timer(),
-        document.getElementById("editor"),
-        window.localStorage,
-        path)
-
-    if (window.localStorage.getItem(path)) {
-        editor.text = window.localStorage.getItem(path)
-    }
-
     return window.requestAnimationFrame(gameLoop)
 }
 
 function update() {
 
-
 }
 
 function draw() {
-    clock.innerHTML = new Date().toString()
 }
 
 
